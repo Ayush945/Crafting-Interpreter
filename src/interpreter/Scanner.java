f230eae12263a1a3edd84db033048d7d4b553427
Ayush <84891062+Ayush945@users.noreply.github.com>
2023-11-10 19:00:46 +0545

    added token, its type and some part to start scanning the tokens

---
+++ src/interpreter/Scanner.java
@@ -0,0 +1,30 @@
+package interpreter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+
+import static interpreter.TokenType.*;
+public class Scanner {
+    private final String source;
+    private final List<Token> tokens=new ArrayList<>();
+    private int start=0;
+    private int current=0;
+    private int line=0;
+    Scanner(String source){
+        this.source=source;
+    }
+    List<Token>scanTokens(){
+        while (!isAtEnd()){
+            start=current;
+            scanToken();
+        }
+        tokens.add(new Token(EOF, "",null,line));
+        return tokens;
+    }
+    private boolean isAtEnd(){
+        return current>=source.length();
+    }
+
+}
